d608bf5079a26c9b2a9b9f111117a3e5e9535f4d
kirpimarmot <cevikkursat@gmail.com>
2022-04-14 23:34:19 +0300

    day38: Inheritance Continue-Method Overriding

---
+++ src/day38_Inheritance/carTask/ParkingLot.java
@@ -0,0 +1,9 @@
+package day38_Inheritance.carTask;
+
+public class ParkingLot {
+
+    Toyota toyota = new Toyota("Camry", 2020, 200000, "Gray", 123456);
+    Tesla tesla = new Tesla("Model S", 2022, 94990, "Red", 0);
+    BMW bmw = new BMW("X5", 2020, 67350, "black", 12000);
+
+}
